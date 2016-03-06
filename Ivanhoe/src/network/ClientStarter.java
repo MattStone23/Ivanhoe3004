bce15f46297b15189e810a1d3f5ef2921cd5149e
MattStone23 <mattstone@cmail.carleton.ca>
2016-03-06 16:16:29 -0500

    Built Jar and tested

--- Ivanhoe/src/network/ClientStarter.java
+++ Ivanhoe/src/network/ClientStarter.java
@@ -17,19 +17,44 @@ public class ClientStarter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Client c = new Client("134.117.28.215",config.PORT);
-		Client c = new Client();
+		String in="temp";
+		Server s = null;
+		Scanner get = new Scanner(System.in); 
+		while (!"JOIN".equals(in)&&!"HOST".equals(in)){
+			System.out.println("Would you like to Host a game a play a game?\nHOST\nJOIN");
+			in = get.nextLine();
+		}
+		
+		if ("HOST".equals(in)){
+			System.out.println("STARTING SERVER");
+			s = new Server();
+			timer.wait(1);
+			in = "0.0.0.0";
+		}
+		else{
+			boolean validIP=false;
+			while (!validIP){
+				System.out.println("Please enter the Server's IP");
+				in = get.nextLine();
+				//TODO validIP = isValidIP(in);
+				validIP=true;
+			}
+		}
+		
+		
+		Client c = new Client(in,config.PORT);
 		timer.wait(1);
 		c.sendMessage("CONNECT");
 //		
 		
-		String in="temp";
-		Scanner get = new Scanner(System.in); 
+		
 		while(!in.equals("quit")){
 			System.out.print("INPUT:");
 			in=get.nextLine();
 			c.sendMessage(in);
 		}
 		c.stop();
+		s.shutdown();
 		
 	}
 
--- Ivanhoe/src/network/Server.java
+++ Ivanhoe/src/network/Server.java
@@ -109,10 +109,11 @@ public class Server implements Runnable {
 			}
 			else{
 				//Lobby is open and not full
-				serverThread.send("ACCEPT");
 				clients.put(serverThread.getID(), serverThread);
-				players.put(serverThread.getID(), clientCount+1);
-				clientCount++; 
+				players.put(serverThread.getID(), new Integer(clientCount+1));
+				clientCount++;
+				System.out.println("ACCEPT|"+players.get(serverThread.getID())+"|"+serverThread.getID());
+				serverThread.send("ACCEPT|"+players.get(serverThread.getID())+"|"+serverThread.getID());
 			}
 		}
 		catch(IOException ioe){
